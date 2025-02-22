b282311c2e80e782dc7c27c3a777f2ea01268681
AlwaysMudgin <193750882+AlwaysMudgin@users.noreply.github.com>
2025-02-21 22:04:00 -0500

    Revise UI from console to buttons

--- script.js
+++ script.js
@@ -9,55 +9,73 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt("Choose: Rock, Paper, Scissors")
-    return String(choice).toLowerCase();
-}
+let computerScore = 0
+let playerScore = 0
 
+// human choice buttons
+const rock = document.querySelector("#rock")
+rock.addEventListener("click", function() {
+    playRound("rock")
+})
 
-function playGame() {
-    let humanScore = 0
-    let computerScore = 0
-    function playRound() {
-        let humanChoice = getHumanChoice()
-        let computerChoice = getComputerChoice()
-        if (humanChoice === "rock") {
-            if (computerChoice === "paper") {
-                ++computerScore
-                console.log("You lose! Paper beats Rock")
-            } else if (computerChoice === "scissor") {
-                ++humanScore
-                console.log("You win! Rock beats Scissors")
-            } else {
-                console.log("Tie! Rock vs Rock")
-            }
-        } else if (humanChoice === "paper") {
-            if (computerChoice === "rock") {
-                ++humanScore
-                console.log("You win! Paper beats Rock")
-            } else if (computerChoice === "scissors") {
-                ++computerScore
-                console.log("You lose! Scissors beats Paper")
-            } else {
-                console.log("Tie! Paper vs Paper")
-            }
-        } else if (humanChoice === "scissors") {
-            if (computerChoice === "rock") {
-                ++computerScore
-                console.log("You lose! Rock beats Scissors")
-            } else if (computerChoice === "paper") {
-                ++humanScore
-                console.log("You win! Scissors beats Paper")
-            } else {
-                console.log("Tie! Scissors vs Scissors")
-            }
+const paper = document.querySelector("#paper")
+paper.addEventListener("click", function() {
+    playRound("paper")
+})
+
+const scissors = document.querySelector("#scissors")
+scissors.addEventListener("click", function() {
+    playRound("scissors")
+})
+
+function playRound (playerChoice) {
+    let computerChoice = getComputerChoice()
+
+    const result = document.querySelector("#result")
+    const pscore = document.querySelector("#playerScore")
+    const cscore = document.querySelector("#computerScore")
+
+    if (playerChoice === "rock") {
+        if (computerChoice === "paper") {
+            ++computerScore
+            result.textContent = "Round lost! Paper beats Rock"
+            cscore.textContent = computerScore
+        } else if (computerChoice === "scissor") {
+            ++playerScore
+            result.textContent = "Round won! Rock beats Scissors"
+            pscore.textContent = playerScore
+        } else {
+            result.textContent = "Tie! Rock vs Rock"
+        }
+    } else if (playerChoice === "paper") {
+        if (computerChoice === "rock") {
+            ++playerScore
+            result.textContent = "Round won! Paper beats Rock"
+            pscore.textContent = playerScore
+        } else if (computerChoice === "scissors") {
+            ++computerScore
+            result.textContent = "Round lost! Scissors beats Paper"
+            cscore.textContent = computerScore
+        } else {
+            result.textContent = "Tie! Paper vs Paper"
+        }
+    } else if (playerChoice === "scissors") {
+        if (computerChoice === "rock") {
+            ++computerScore
+            result.textContent = "Round lost! Rock beats Scissors"
+            cscore.textContent = computerScore
+        } else if (computerChoice === "paper") {
+            ++playerScore
+            result.textContent = "Round won! Scissors beats Paper"
+            pscore.textContent = playerScore
+        } else {
+            result.textContent = "Tie! Scissors vs Scissors"
         }
     }
-    playRound()
-    playRound()
-    playRound()
-    playRound()
-    playRound()
+    if(playerScore >= 5) {
+        alert("You win!")
+    }
+    if(computerScore >= 5) {
+        alert("You lose!")
+    }
 }
-
-playGame()
